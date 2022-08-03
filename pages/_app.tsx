a316985e98ac42a91a255f25cbeabafad323d465
0xVict0r <74269639+0xVict0r@users.noreply.github.com>
2022-08-03 10:09:14 +0200

    fixed ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,14 +20,12 @@ function MyApp({ Component, pageProps }: AppProps) {
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
 
-      <Script id="google-analytics" strategy="lazyOnload">
+      <Script strategy="lazyOnload">
         {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
                 `}
       </Script>
       <NextSeo
